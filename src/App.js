e2e644b6eaea451a921f5d97953559bb5880bb98
niroavram <69424993+niroavram@users.noreply.github.com>
2021-11-08 08:00:01 +0200

    ש

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ function App() {
         a[index].isChosen=true
       }else{
         if(cardsChosen[0].value===card.value){
-          
+          a[index].isChosen=true
           setCardsChosen(null)
         }else{
           
